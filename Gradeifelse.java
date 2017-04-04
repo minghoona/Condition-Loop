f92c08392014ee38b11ea1903ae3b4235cba3650
minghoona <trkmh0718@gmail.com>
2017-04-04 21:07:09 +0900

    조건과 반복의 자바 소스 추가
    조건과 반복의 자바 소스 추가

---
+++ Gradeifelse.java
@@ -0,0 +1,25 @@
+package control.ifcondition;
+
+import java.util.Scanner;
+
+public class Gradeifelse {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("성적 입력 : ");
+		int point = input.nextInt();
+		char grade;
+		
+		if(90<=point)
+			grade = 'A';
+		else if(80<=point)
+			grade = 'B';
+		else if(70<=point)
+			grade = 'C';
+		else
+			grade = 'F';
+		System.out.println("학점 :"+ grade);
+
+	}
+
+}
